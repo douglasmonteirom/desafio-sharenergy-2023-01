251f060fe94f932cfddf56c6aa6fa6ea466bacc8
douglas <douglasmonteiro.m@gmail.com>
2023-01-06 12:01:05 -0300

    feature: cria model do client

---
+++ back/src/domain/models/client.ts
@@ -0,0 +1,8 @@
+export type ClientModel = {
+  createdAt: Date;
+  name: string | undefined;
+  email: string | undefined;
+  phone: string | undefined;
+  adress: string | undefined;
+  cpf: number | undefined;
+}
