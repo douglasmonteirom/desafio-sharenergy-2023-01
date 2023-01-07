a57084f01ff5f8d4f6f87e7263f26e1af89218eb
douglas <douglasmonteiro.m@gmail.com>
2023-01-07 11:11:48 -0300

    feature: implementa função add no ClientsMongoRepository

--- back/src/db/mongodb/clients-mongo-repository.ts
+++ back/src/db/mongodb/clients-mongo-repository.ts
@@ -1,8 +1,13 @@
+import { AddClientRepository } from "../../data/protocols/db/client/add-client-repository";
 import { ReadAllClientsRepository } from "../../data/protocols/db/client/read-all-clients-repository";
+import { AddClient } from "../../domain/usecases/add-client";
 import Client from "./models/client";
 
-export class ClientsMongoRepository implements ReadAllClientsRepository {
+export class ClientsMongoRepository implements ReadAllClientsRepository, AddClientRepository {
   constructor() { }
+  async add(data: AddClient.Params): Promise<void> {
+    await Client.create(data)
+  }
   async readAll(): Promise<ReadAllClientsRepository.Result> {
     return Client.find()
   }
