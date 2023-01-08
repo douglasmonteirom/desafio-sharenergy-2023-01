bcc91233895c846ded6bc76694cd2023969cb0eb
douglas <douglasmonteiro.m@gmail.com>
2023-01-08 17:55:47 -0300

    feature: cria interface UpdateClient

---
+++ back/src/domain/usecases/update-client.ts
@@ -0,0 +1,13 @@
+import { ClientModel } from "../models/client"
+
+export interface UpdateClient {
+  update(data: UpdateClient.Params): Promise<UpdateClient.Result>
+}
+
+export namespace UpdateClient {
+  export type Result = ClientModel | null
+  export type Params = {
+    id: string,
+    client: ClientModel
+  }
+}
