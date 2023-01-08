602c6e70a53262c5e03508002aa62b5939183836
douglas <douglasmonteiro.m@gmail.com>
2023-01-08 16:12:16 -0300

    feature: implementa função remove no ClientsMongoRepository

--- back/src/db/mongodb/clients-mongo-repository.ts
+++ back/src/db/mongodb/clients-mongo-repository.ts
@@ -1,12 +1,17 @@
 import { AddClientRepository } from "../../data/protocols/db/client/add-client-repository";
 import { ReadAllClientsRepository } from "../../data/protocols/db/client/read-all-clients-repository";
 import { ReadOneClientRepository } from "../../data/protocols/db/client/read-one-client-repository";
+import { RemoveClientRepository } from "../../data/protocols/db/client/remove-client-repository";
 import { AddClient } from "../../domain/usecases/add-client";
 import { ReadOneClient } from "../../domain/usecases/read-one-client";
+import { RemoveClient } from "../../domain/usecases/remove-client";
 import Client from "./models/client";
 
-export class ClientsMongoRepository implements ReadAllClientsRepository, AddClientRepository, ReadOneClientRepository {
+export class ClientsMongoRepository implements ReadAllClientsRepository, AddClientRepository, ReadOneClientRepository, RemoveClientRepository {
   constructor() { }
+  async remove(data: RemoveClient.Params): Promise<void> {
+    await Client.deleteOne({ __id: data.id })
+  }
   async read(data: ReadOneClient.Params): Promise<ReadOneClientRepository.Result> {
     return await Client.findOne({ _id: data.id })
   }
