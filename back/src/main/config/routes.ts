8606c18e16d665a944f456c9d736339e3a3c5c41
douglas <douglasmonteiro.m@gmail.com>
2023-01-08 13:55:18 -0300

    refactor: remove console.log do codigo

--- back/src/main/config/routes.ts
+++ back/src/main/config/routes.ts
@@ -5,10 +5,7 @@ import { join } from 'path'
 export default (app: Express): void => {
   const router = Router()
   app.use('/api', router)
-  const tsd = join(__dirname, '../routes')
-  console.log(tsd)
   readdirSync(join(__dirname, '../routes')).map(async file => {
-    console.log(file)
     if (!file.endsWith('.map')) {
       (await import(`../routes/${file}`)).default(router)
     }
