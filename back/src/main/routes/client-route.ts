00a5e08833d28deb78e16da106b43833791a90c1
douglas <douglasmonteiro.m@gmail.com>
2023-01-08 17:23:55 -0300

    feature: cria rota delete para remover client pelo id

--- back/src/main/routes/client-route.ts
+++ back/src/main/routes/client-route.ts
@@ -3,9 +3,11 @@ import { adaptRoute } from "../adapters/express-route-adapter"
 import { makeAddClientController } from "../factories/controllers/add-client-controller-factory"
 import { makeReadAllClientsController } from "../factories/controllers/read-all-clients-controller-factory"
 import { makeReadOneClientController } from "../factories/controllers/read-one-client-controller-factory"
+import { makeRemoveClientController } from "../factories/controllers/remove-client-controller-factory"
 
 export default (router: Router): void => {
   router.post('/client', adaptRoute(makeAddClientController()))
   router.get('/client', adaptRoute(makeReadAllClientsController()))
   router.get('/client/:id', adaptRoute(makeReadOneClientController()))
+  router.delete('/client/:id', adaptRoute(makeRemoveClientController()))
 }
