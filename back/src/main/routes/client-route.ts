7edc9471f6bf158eb9be9f6499b7d4986841c59c
douglas <douglasmonteiro.m@gmail.com>
2023-01-08 15:43:15 -0300

    feature: cria rota get para encontrar client pelo id

--- back/src/main/routes/client-route.ts
+++ back/src/main/routes/client-route.ts
@@ -2,8 +2,10 @@ import { Router } from "express"
 import { adaptRoute } from "../adapters/express-route-adapter"
 import { makeAddClientController } from "../factories/controllers/add-client-controller-factory"
 import { makeReadAllClientsController } from "../factories/controllers/read-all-clients-controller-factory"
+import { makeReadOneClientController } from "../factories/controllers/read-one-client-controller-factory"
 
 export default (router: Router): void => {
   router.post('/client', adaptRoute(makeAddClientController()))
   router.get('/client', adaptRoute(makeReadAllClientsController()))
+  router.get('/client/:id', adaptRoute(makeReadOneClientController()))
 }
