8c994d0c2f8e417e22d2c71b7d5b10c933ebd361
douglas <douglasmonteiro.m@gmail.com>
2023-01-08 18:02:38 -0300

    feature: implementa função update no ClientsMongoRepository

--- back/src/db/mongodb/clients-mongo-repository.ts
+++ back/src/db/mongodb/clients-mongo-repository.ts
@@ -2,13 +2,18 @@ import { AddClientRepository } from "../../data/protocols/db/client/add-client-r
 import { ReadAllClientsRepository } from "../../data/protocols/db/client/read-all-clients-repository";
 import { ReadOneClientRepository } from "../../data/protocols/db/client/read-one-client-repository";
 import { RemoveClientRepository } from "../../data/protocols/db/client/remove-client-repository";
+import { UpdateClientRepository } from "../../data/protocols/db/client/update-client-repository";
 import { AddClient } from "../../domain/usecases/add-client";
 import { ReadOneClient } from "../../domain/usecases/read-one-client";
 import { RemoveClient } from "../../domain/usecases/remove-client";
+import { UpdateClient } from "../../domain/usecases/update-client";
 import Client from "./models/client";
 
-export class ClientsMongoRepository implements ReadAllClientsRepository, AddClientRepository, ReadOneClientRepository, RemoveClientRepository {
+export class ClientsMongoRepository implements ReadAllClientsRepository, AddClientRepository, ReadOneClientRepository, RemoveClientRepository, UpdateClientRepository {
   constructor() { }
+  async update(data: UpdateClient.Params): Promise<UpdateClient.Result> {
+    return await Client.findOneAndUpdate({ _id: data.id }, { ...data.client })
+  }
   async remove(data: RemoveClient.Params): Promise<RemoveClientRepository.Result> {
     return await Client.findOneAndRemove({ _id: data.id })
   }
