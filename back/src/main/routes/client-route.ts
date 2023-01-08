9e1a0920b7ecb5b0cfd60b4e6ed96b46aad2ce1f
douglas <douglasmonteiro.m@gmail.com>
2023-01-08 13:56:24 -0300

    feature: cria rota post para adicionar cliente

--- back/src/main/routes/client-route.ts
+++ back/src/main/routes/client-route.ts
@@ -1,7 +1,9 @@
 import { Router } from "express"
 import { adaptRoute } from "../adapters/express-route-adapter"
+import { makeAddClientController } from "../factories/controllers/add-client-controller-factory"
 import { makeReadAllClientsController } from "../factories/controllers/read-all-clients-controller-factory"
 
 export default (router: Router): void => {
+  router.post('/client', adaptRoute(makeAddClientController()))
   router.get('/client', adaptRoute(makeReadAllClientsController()))
 }
