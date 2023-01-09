bc8ebb24a00d53f443746a533e396d74f5ec7803
douglas <douglasmonteiro.m@gmail.com>
2023-01-09 19:48:20 -0300

    feature: cria e implementa encripter e decripter com jasonwebtoken

---
+++ back/src/infra/cryptography/jwt-adapter.ts
@@ -0,0 +1,15 @@
+import jwt from 'jsonwebtoken'
+import { Decrypter } from '../../data/protocols/cryptography/decrypter'
+import { Encrypter } from '../../data/protocols/cryptography/encrypter'
+
+export class JwtAdapter implements Encrypter, Decrypter {
+  constructor(private readonly secret: string) { }
+
+  async encrypt(plaintext: string): Promise<string> {
+    return jwt.sign({ id: plaintext }, this.secret)
+  }
+
+  async decrypt(ciphertext: string): Promise<string> {
+    return jwt.verify(ciphertext, this.secret) as any
+  }
+}
