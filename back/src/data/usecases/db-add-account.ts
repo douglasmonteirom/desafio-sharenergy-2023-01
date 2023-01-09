5bef2da3caff15c47d3e36dfd53ecfe5114fd055
douglas <douglasmonteiro.m@gmail.com>
2023-01-09 09:24:23 -0300

    refactor: lembra futuras implementações

--- back/src/data/usecases/db-add-account.ts
+++ back/src/data/usecases/db-add-account.ts
@@ -5,6 +5,9 @@ export class DbAddAccount implements AddAccount {
   constructor(private readonly addAccount: AddAccountRepositpry) { }
 
   async add(account: AddAccount.Params): Promise<boolean> {
+    // adicionar validação de campos
+    // adicionar validação de email em uso
+    // encriptar senha
     return this.addAccount.add(account)
   }
 }
