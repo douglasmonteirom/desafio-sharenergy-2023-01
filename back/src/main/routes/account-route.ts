4d291f567047b558a6e410d7dcae23900a28b650
douglas <douglasmonteiro.m@gmail.com>
2023-01-09 20:32:23 -0300

    feature: adiciona rota post login

--- back/src/main/routes/account-route.ts
+++ back/src/main/routes/account-route.ts
@@ -1,7 +1,9 @@
 import { Router } from "express"
 import { adaptRoute } from "../adapters/express-route-adapter"
+import { makeLoginController } from "../factories/controllers/login-controller-factory"
 import { makeRegisterController } from "../factories/controllers/register-controller-factory"
 
 export default (router: Router): void => {
   router.post('/register', adaptRoute(makeRegisterController()))
+  router.post('/login', adaptRoute(makeLoginController()))
 }
