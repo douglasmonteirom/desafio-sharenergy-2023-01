1805c71480467663c45ced236addb69b5e2ea2ac
douglas <douglasmonteiro.m@gmail.com>
2023-01-09 20:02:14 -0300

    feature: adiciona makeDbAuthentication no factory do registerController

--- back/src/main/factories/controllers/register-controller-factory.ts
+++ back/src/main/factories/controllers/register-controller-factory.ts
@@ -1,8 +1,9 @@
 import { RegisterController } from "../../../presentation/controllers/register-controller";
 import { Controller } from "../../../presentation/protocols/controller";
 import { makeDbAddAccount } from "../usecases/add-account-factory";
+import { makeDbAuthentication } from "../usecases/authentication-factory";
 import { makeRegisterValidation } from "./register-validation-factory";
 
 export const makeRegisterController = (): Controller => {
-  return new RegisterController(makeDbAddAccount(), makeRegisterValidation())
+  return new RegisterController(makeDbAddAccount(), makeRegisterValidation(), makeDbAuthentication())
 }
--- back/src/main/factories/usecases/add-account-factory.ts
+++ back/src/main/factories/usecases/add-account-factory.ts
@@ -1,6 +1,6 @@
 import { DbAddAccount } from "../../../data/usecases/db-add-account";
 import { AddAccount } from "../../../domain/usecases/add-account";
-import { BcryptAdapter } from "../../../infra/cryptography/bcrypt.adapter";
+import { BcryptAdapter } from "../../../infra/cryptography/bcrypt-adapter";
 import { AccountMongoRepository } from "../../../infra/db/mongodb/account-mongo-repository";
 
 export const makeDbAddAccount = (): AddAccount => {
