f22340e91e0d4b6d392b823042670d85919af153
douglas <douglasmonteiro.m@gmail.com>
2023-01-09 19:57:09 -0300

    feature: implementa readByEmail no AccountMongoRepository

--- back/src/infra/db/mongodb/account-mongo-repository.ts
+++ back/src/infra/db/mongodb/account-mongo-repository.ts
@@ -1,11 +1,16 @@
-import { AddAccountRepositpry } from "../../data/protocols/db/account/add-account-repository";
-import { CheckAccountByEmailRepository } from "../../data/protocols/db/account/check-account-by-email-repository";
-import { AddAccount } from "../../domain/usecases/add-account";
+import { AddAccountRepositpry } from "../../../data/protocols/db/account/add-account-repository";
+import { CheckAccountByEmailRepository } from "../../../data/protocols/db/account/check-account-by-email-repository";
+import { ReadAccountByEmailRepository } from "../../../data/protocols/db/account/read-account-by-email-repository";
+import { AddAccount } from "../../../domain/usecases/add-account";
 import User from "./models/user";
 
-export class AccountMongoRepository implements AddAccountRepositpry, CheckAccountByEmailRepository {
+export class AccountMongoRepository implements AddAccountRepositpry, CheckAccountByEmailRepository, ReadAccountByEmailRepository {
+  async readByEmail(email: string): Promise<ReadAccountByEmailRepository.Result> {
+    const account = await User.findOne({ email }).select(['password', 'name'])
+    return { id: account?.id, name: account?.name, password: account?.password }
+  }
   async checkByEmail(email: string): Promise<boolean> {
-    const account = await User.findOne({ email })
+    const account = await User.findOne({ email });
     return account !== null
   }
   async add(data: AddAccount.Params): Promise<boolean> {
