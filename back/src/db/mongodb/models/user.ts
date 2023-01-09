f0f4035e6b8f28cb020bf35cebe06486752e0275
douglas <douglasmonteiro.m@gmail.com>
2023-01-09 09:21:28 -0300

    feature: cria model user no mongoose

---
+++ back/src/db/mongodb/models/user.ts
@@ -0,0 +1,36 @@
+import bcrypt from 'bcryptjs';
+import { mongoose } from "../index";
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    require: true
+  },
+  email: {
+    type: String,
+    unique: true,
+    require: true,
+    lowercase: true
+  },
+  password: {
+    type: String,
+    require: true,
+    select: false
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+})
+
+UserSchema.pre('save', async function (next) {
+  if (this.password) {
+    const hash = await bcrypt.hash(this.password, 10)
+    this.password = hash
+  }
+  next()
+})
+
+const User = mongoose.model('User', UserSchema)
+
+export default User
