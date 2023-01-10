2cfb55e098ca6191cd1f5ca7a1fe9c4403fd04ef
douglas <douglasmonteiro.m@gmail.com>
2023-01-10 08:57:51 -0300

    feature: adiciona middleware de autenticação nas rotas de client

--- back/src/main/routes/client-route.ts
+++ back/src/main/routes/client-route.ts
@@ -5,8 +5,10 @@ import { makeReadAllClientsController } from "../factories/controllers/read-all-
 import { makeReadOneClientController } from "../factories/controllers/read-one-client-controller-factory"
 import { makeRemoveClientController } from "../factories/controllers/remove-client-controller-factory"
 import { makeUpdateClientController } from "../factories/controllers/update-client-controller-factory"
+import { auth } from "../middlewares/auth"
 
 export default (router: Router): void => {
+  router.use(auth)
   router.post('/client', adaptRoute(makeAddClientController()))
   router.get('/client', adaptRoute(makeReadAllClientsController()))
   router.get('/client/:id', adaptRoute(makeReadOneClientController()))
